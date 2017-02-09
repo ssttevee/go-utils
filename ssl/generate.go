39fd32a04110cb724c3f626d914fa1c4dbb19ff9
ssttevee <stevyo99@yahoo.ca>
2017-02-08 23:56:53 -0800

    cleaned up ssl package

--- ssl/generate.go
+++ ssl/generate.go
@@ -1,3 +1,4 @@
+// Package ssl self signs SSL certificates to be used for development use
 package ssl
 
 import (
@@ -6,53 +7,45 @@ import (
 	"crypto/rand"
 	"crypto/x509"
 	"math/big"
-	"crypto/x509/pkix"
-	"fmt"
 	"time"
 )
 
-func NewSelfSignedTLSConfig(org string, duration time.Duration) (*tls.Config, error) {
-	cert, err := tls.LoadX509KeyPair("/secrets/ssl/tls.crt", "/secrets/ssl/tls.key")
+// NewSelfSignedTLSConfig returns a new tls config with a self signed certificate
+func NewSelfSignedTLSConfig() (*tls.Config, error) {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
-		key, err := rsa.GenerateKey(rand.Reader, 2048)
-		if err != nil {
-			return nil, fmt.Errorf("failed to generate private key: %v", err)
-		}
+		return nil, err
+	}
 
-		notBefore := time.Now()
-		notAfter := notBefore.Add(duration)
+	notBefore := time.Now()
+	notAfter := notBefore.Add(time.Hour)
 
-		serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-		serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-		if err != nil {
-			return nil, fmt.Errorf("failed to generate serial number: %s", err)
-		}
+	serialNumberMax := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberMax)
+	if err != nil {
+		return nil, err
+	}
 
-		template := &x509.Certificate{
-			SerialNumber: serialNumber,
-			Subject: pkix.Name{
-				Organization: []string{org},
-			},
-			NotBefore: notBefore,
-			NotAfter: notAfter,
+	template := &x509.Certificate{
+		SerialNumber: serialNumber,
+		NotBefore: notBefore,
+		NotAfter: notAfter,
 
-			KeyUsage: x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-			ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-			BasicConstraintsValid: true,
-		}
+		KeyUsage: x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
 
-		derBytes, err := x509.CreateCertificate(rand.Reader, template, template, &key.PublicKey, key)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create certificate: %s", err)
-		}
-
-		cert.Certificate = [][]byte{derBytes}
-		cert.PrivateKey = key
-		cert.Leaf = template
+	derBytes, err := x509.CreateCertificate(rand.Reader, template, template, &key.PublicKey, key)
+	if err != nil {
+		return nil, err
 	}
 
 	return &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		InsecureSkipVerify: true,
+		Certificates: []tls.Certificate{{
+			Certificate: [][]byte{derBytes},
+			PrivateKey: key,
+			Leaf: template,
+		}},
 	}, nil
 }
